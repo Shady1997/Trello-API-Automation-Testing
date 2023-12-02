76634ab0f55e165d52f7786ef0d2e92ea54f7820
Shady Gomaa <shady.gomaa@idemia.com>
2023-12-02 14:11:22 +0200

    new update

--- Trello's APIs testing/run-using-newman.bat
+++ Trello's APIs testing/run-using-newman.bat
@@ -1,3 +1,3 @@
-newman run "%cd%\Trello-APIs.postman_collection.json" -e "%cd%\Trello-APIs.postman_environment.json" --reporters=cli,htmlextra
+newman run "%cd%\Test-TRELLO-REST-APIs.postman_collection.json" -e "%cd%\Trello-APIs.postman_environment.json" --reporters=cli,htmlextra
 echo succss
 pause
